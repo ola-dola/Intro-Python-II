643d683492c5c4385111926cb09ed12b9050c287
ola-dola <oredolaolamide@gmail.com>
2020-06-03 14:46:15 +0100

    instatiate some Item objects

--- src/adv.py
+++ src/adv.py
@@ -1,25 +1,35 @@
 from room import Room
 from player import Player
-
+from item import Item
 # Declare all the rooms
+
+water = Item("water", "good for the journey")
+coins = Item("coins", "shines bright like a diamond")
+sword = Item("sword", "cuts you, saves you")
+chain = Item("chain", "can tie you down, you can tie your enemy")
+naira = Item("naira", "some say it is worthless. I will take 382mil anyday tho (o__0)")
+guitar = Item("naira", "who is the best to do it? KSA or Fatai Rolling Dollar?")
+sanitizer = Item("sanitizer", "wash off all the virus, before they mess you up")
+chloro = Item("chloroquine", "Some say do not believe trump. Do you think it is fair to drag drugs into politics?")
+gloves = Item("gloves", "Do you have what it takes to beat Wilder?")
 
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+                     "North of you, the cave mount beckons", water, sanitizer),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+passages run north and east.""", chain, naira, chloro),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.""", sword, chloro, gloves),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+to north. The smell of gold permeates the air.""", coins, sword, gloves),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers. The only exit is to the south.""", water, coins, sanitizer, naira, guitar, chloro),
 }
 
 
@@ -58,6 +68,10 @@ player = Player("Ola", room["outside"])
 while True:
     print("You are currently in " + player.current_room.name)
     print(player.current_room.description + "\n")
+
+    print("Items available to you in this room are:")
+    for item in player.current_room.items:
+        print("\t" + item.name)
     
     dir = input("Pick a direction to change room:  ")
 
